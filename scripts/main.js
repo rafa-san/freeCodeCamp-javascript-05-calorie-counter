a36315ccd066d1772e47f1dae52e05c1258345fb
Rafael <88090582+rafa-san@users.noreply.github.com>
2024-09-14 10:24:58 -0300

    Adição de novas linhas de código

--- scripts/main.js
+++ scripts/main.js
@@ -15,5 +15,23 @@ function cleanInputString(str) {
 //console.log(cleanInputString("+-99"));
 
 function isInvalidInput(str) {
-  const regex = /e/;
+  //const regex = /[0-9]+e[0-9]+/i; é possível simplificar isso
+  const regex = /\d+e\d+/i;
+  return str.match(regex);
+}
+
+//console.log(isInvalidInput("1e3")); // [ '1e3', index: 0, input: '1e3', groups: undefined ]
+//console.log(isInvalidInput("10")); // null
+
+function addEntry() {
+  // targetId = "#" + entryDropdown.value;
+  // const targetInputContainer = document.querySelector(targetId + " .input-container");
+  const targetInputContainer = document.querySelector(`#${entryDropdown.value} .input-container`);
+  const entryNumber = targetInputContainer.querySelectorAll('input[type="text"]').length;
+  const HTMLString = `
+  <label for="${entryDropdown.value}-${entryNumber}-name">Entry ${entryNumber} Name</label>
+  <input type="text" placeholder="Name" id="${entryDropdown.value}-${entryNumber}-name" />
+  <label for="${entryDropdown.value}-${entryNumber}-calories">Entry ${entryNumber} Calories</label>
+  <input type="number" min="0" id="${entryDropdown.value}-${entryNumber}-calories" placeholder="Calories" />
+  `;
 }
