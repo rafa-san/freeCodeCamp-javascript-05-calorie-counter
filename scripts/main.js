2c1436a7cad432410294f8859629b3c195137f54
Rafael <88090582+rafa-san@users.noreply.github.com>
2024-10-02 10:16:20 -0300

    Adição de novas linhas de código

--- scripts/main.js
+++ scripts/main.js
@@ -62,7 +62,14 @@ function calculateCalories(e) {
   const consumedCalories = breakfastCalories + lunchCalories + dinnerCalories + snacksCalories;
   const remainingCalories  = (budgetCalories  - consumedCalories) + exerciseCalories;
   const surplusOrDeficit = remainingCalories < 0 ? "Surplus":"Deficit";
-  output.innerHTML = ``;
+  output.innerHTML = `
+  <span class="${surplusOrDeficit.toLowerCase()}">${Math.abs(remainingCalories)} Calorie ${surplusOrDeficit}</span>
+  <hr>
+  <p>${budgetCalories} Calories Budgeted</p>
+  <p>${consumedCalories} Calories Consumed</p>
+  <p>${exerciseCalories} Calories Burned</p>
+  `;
+  output.classList.remove("hide");
 }
 
 function getCaloriesFromInputs(list) {
@@ -80,4 +87,16 @@ function getCaloriesFromInputs(list) {
   return calories;
 }
 
+function clearForm() {
+  const inputContainers = Array.from(document.querySelectorAll('.input-container'));
+  for (let container of inputContainers) {
+    container.innerHTML = "";
+  }
+  budgetNumberInput.value = "";
+  output.innerText = "";
+  output.classList.add("hide");
+}
+
 addEntryButton.addEventListener("click", addEntry);
+calorieCounter.addEventListener("submit", calculateCalories);
+clearButton.addEventListener("click", clearForm);
