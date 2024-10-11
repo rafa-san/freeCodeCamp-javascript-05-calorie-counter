e08e2f583eac8badad948a634a3e225e41c52de4
Rafael <88090582+rafa-san@users.noreply.github.com>
2024-10-11 09:49:07 -0300

    style: remove comments

--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,4 @@
 const calorieCounter = document.getElementById('calorie-counter');
-//calorieCounter.style.backgroundColor = "red";
 const budgetNumberInput = document.getElementById('budget');
 const entryDropdown = document.getElementById('entry-dropdown');
 const addEntryButton = document.getElementById('add-entry');
@@ -8,32 +7,27 @@ const output = document.getElementById('output');
 let isError = false;
 
 function cleanInputString(str) {
-  //console.log("original string: ", str);
   const regex = /[+-\s]/g;
   return str.replace(regex, '');
 }
-//console.log(cleanInputString("+-99"));
 
 function isInvalidInput(str) {
-  //const regex = /[0-9]+e[0-9]+/i; é possível simplificar isso
   const regex = /\d+e\d+/i;
   return str.match(regex);
 }
 
-//console.log(isInvalidInput("1e3")); // [ '1e3', index: 0, input: '1e3', groups: undefined ]
-//console.log(isInvalidInput("10")); // null
-
 function addEntry() {
-  // targetId = "#" + entryDropdown.value;
-  // const targetInputContainer = document.querySelector(targetId + " .input-container");
   const targetInputContainer = document.querySelector(`#${entryDropdown.value} .input-container`);
   const entryNumber = targetInputContainer.querySelectorAll('input[type="text"]').length + 1;
   const HTMLString = `
   <label for="${entryDropdown.value}-${entryNumber}-name">Entry ${entryNumber} Name</label>
   <input type="text" placeholder="Name" id="${entryDropdown.value}-${entryNumber}-name" />
   <label for="${entryDropdown.value}-${entryNumber}-calories">Entry ${entryNumber} Calories</label>
-  <input type="number" min="0" id="${entryDropdown.value}-${entryNumber}-calories" placeholder="Calories" />
-  `;
+  <input 
+    type="number" 
+    min="0" 
+    id="${entryDropdown.value}-${entryNumber}-calories" placeholder="Calories" 
+  />`;
   targetInputContainer.insertAdjacentHTML("beforeend", HTMLString);
 }
 
